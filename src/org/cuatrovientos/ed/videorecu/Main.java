61968cfeec39a600a6c3189cf045795988e27d19
IkerSan <cosasquequieres@gmail.com>
2025-04-13 20:20:52 +0200

    Primera versión de main

--- src/org/cuatrovientos/ed/videorecu/Main.java
+++ src/org/cuatrovientos/ed/videorecu/Main.java
@@ -1,17 +1,33 @@
 package org.cuatrovientos.ed.videorecu;
-
+import java.util.*;
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
 		String tipoOperacion;
 		Double numero1;
 		Double numero2;
+		Double resultado;
 		
 		System.out.println("CALCULADORA SIMPLE");
 		System.out.println("¿Qué tipo de operación quieres realizar?");
-		System.out.println("Suma / Resta / Multiplicacion / Division");
+		System.out.print("Suma / Resta / Multiplicacion / Division: ");
 		
+		tipoOperacion = scanner.nextLine();
+		
+		System.out.print("Introduce el número 1: ");
+		
+		numero1 = scanner.nextDouble();
+		
+		System.out.print("Introduce el número 2: ");
+		
+		numero2 = scanner.nextDouble();
+		
+		Calculadora operacion = new Calculadora();
+		resultado = operacion.calcular(tipoOperacion, numero1, numero2);
+		
+		System.out.print("Resultado: " + resultado);
 	}
 
 }

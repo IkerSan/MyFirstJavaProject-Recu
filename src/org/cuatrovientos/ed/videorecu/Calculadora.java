b959b669a38a123ad4960b9bd874ac56a5eaa8cc
IkerSan <cosasquequieres@gmail.com>
2025-04-13 20:10:14 +0200

    Clase calculadora completada

--- src/org/cuatrovientos/ed/videorecu/Calculadora.java
+++ src/org/cuatrovientos/ed/videorecu/Calculadora.java
@@ -1,5 +1,19 @@
 package org.cuatrovientos.ed.videorecu;
 
 public class Calculadora {
-
+	
+	public double calcular(String operacion, double num1, double num2) {
+        switch (operacion.toLowerCase()) {
+            case "suma":
+                return num1 + num2;
+            case "resta":
+                return num1 - num2;
+            case "multiplicacion":
+                return num1 * num2;
+            case "division":
+                return num1 / num2;
+            default:
+                throw new IllegalArgumentException(operacion + "no es válido");
+        }
+	}
 }
--- src/org/cuatrovientos/ed/videorecu/Main.java
+++ src/org/cuatrovientos/ed/videorecu/Main.java
@@ -3,8 +3,15 @@ package org.cuatrovientos.ed.videorecu;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String tipoOperacion;
+		Double numero1;
+		Double numero2;
+		
+		System.out.println("CALCULADORA SIMPLE");
+		System.out.println("¿Qué tipo de operación quieres realizar?");
+		System.out.println("Suma / Resta / Multiplicacion / Division");
+		
 	}
 
 }
